bf2bc8fa6530c6331cc4f40206cb866674e14eb1
ammarahmed <ammarahmed@wirelessprv-10-192-239-90.near.illinois.edu>
2017-11-07 16:36:32 -0600

    Finished lab 10.

--- src/main/java/EmployeeDatabase.java
+++ src/main/java/EmployeeDatabase.java
@@ -15,7 +15,7 @@ public class EmployeeDatabase {
     /**
      * List of employees.
      */
-    public List<Employee> employees;
+    private List<Employee> employees;
 
     /**
      * Constructor which initializes the employees list.
@@ -30,10 +30,10 @@ public class EmployeeDatabase {
     /**
      * Returns the manager for the given employee.
      *
-     * @param employee
-     * @return
+     * @param employee yes
+     * @return manager
      */
-    Employee findManager(final Employee employee) {
+    public Employee findManager(final Employee employee) {
         Employee manager = null;
         for (int i = 0; i < employees.size(); i++) {
             if (employees.get(i).getName() == employee.getManager()) {
@@ -56,6 +56,18 @@ public class EmployeeDatabase {
         /*
          * Implement this function
          */
+        Employee manager = null;
+        Employee person = employee;
+        int count = 1;
+        if (employee.getName() == "Veronica") {
+            return 0;
+        }
+        while (findManager(person).getName() != "Veronica") {
+            manager = findManager(person);
+            person = manager;
+            count++;
+        }
+        return count;
     }
 
     /**
@@ -70,6 +82,16 @@ public class EmployeeDatabase {
         /*
          * Implement this function
          */
+        int count = 0;
+        for (int i = 0; i < employees.size(); i++) {
+            if (employees.get(i).getManager() == employee.getName()) {
+                count++;
+                count += countEmployeesUnder(employees.get(i));
+            }
+        }
+
+        return count;
+
     }
 
     /**
